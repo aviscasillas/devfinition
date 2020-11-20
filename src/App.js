d9c933600e04962713221d602debe5768a1db537
Albert Viscasillas <aviscasillas@gmail.com>
2020-11-20 15:19:56 +0100

    Change app logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+        <h2>
           Domain-Driven Design (DDD)
-        </h1>
+        </h2>
+        <h1>¯\_(ツ)_/¯</h1>
         <a
           className="App-link"
           href="https://circleci.com/blog/exploring-domain-driven-design-at-circleci/?utm_medium=SEM&utm_source=gnb&utm_campaign=SEM-gnb-DSA-Eng-ni&utm_content=&utm_term=dynamicSearch-&gclid=CjwKCAiAzNj9BRBDEiwAPsL0d2eKoKA1AlVYlyvMu-k-EdVPApm9q2g5EU8STZxXrp3a11txB50unxoCZMwQAvD_BwE"
