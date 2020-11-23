b71d54127590ac351856cabbab952950b5cd213d
Albert Viscasillas <aviscasillas@gmail.com>
2020-11-23 19:11:22 +0100

    Fix failing test on random devfinitions rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+
+import devfinitions from './devfinitions.json';
 import App from './App';
 
-test('renders DDD', () => {
+test('renders a random devfinition', () => {
+  jest.spyOn(Math, 'random').mockImplementation(() => 0);
+
   render(<App />);
-  const linkElement = screen.getByText(/DDD/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(devfinitions[0]["name"]);
+  expect(nameElement).toBeInTheDocument();
 });
+
