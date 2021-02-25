4818608e87fd10bf955bd11d44134c0f5c9cad7a
Albert Viscasillas <aviscasillas@gmail.com>
2021-02-25 15:22:25 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import devfinitions from './data/devfinitions.json';
 import './App.css';
 
 function App() {
+  var more = () => {};
   var current = devfinitions[Math.floor(Math.random()*devfinitions.length)];
 
   return (
     <div className="App">
-      <a href="https://github.com/aviscasillas/devfinition" target="blank">
-        <img src={forkme} className="App-forkme" alt="Fork me on GitHub" />
-      </a>
       <header className="App-header">
-        <h2>
+        <a href="https://github.com/aviscasillas/devfinition" target="blank">
+          <img src={forkme} className="App-forkme" alt="Fork me on GitHub" />
+        </a>
+        <h1>
           {current["name"]}
-        </h2>
-        <h1>¯\_(ツ)_/¯</h1>
+        </h1>
         <a className="App-link" href="{current['link']}" target="_blank" rel="noopener noreferrer">
           {current["definition"]}
         </a>
+        <div className="App-button" onClick={more}><h1>+</h1></div>
       </header>
     </div>
   );
